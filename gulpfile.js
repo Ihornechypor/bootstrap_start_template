99d74e040b0acfe7a439bc0e1359ed3a7e7546c3
IhorNechypor <chip_a@ukr.net>
2019-04-14 09:58:41 +0200

    library add

--- gulpfile.js
+++ gulpfile.js
@@ -38,12 +38,12 @@ gulp.task('styles', function() {
 
 gulp.task('scripts', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/jquery/distr/jquery.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
-	.pipe(gulp.dest('dist/js'))
+	.pipe(gulp.dest('distr/js'))
 	.pipe(browserSync.reload({ stream: true }))
 });
 
